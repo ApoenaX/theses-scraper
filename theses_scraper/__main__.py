67afd7946492b6a980f6915c990a693cf5727691
Jessica Sousa <jessicasousa.pc@gmail.com>
2024-10-26 22:49:27 -0300

    ✨ feat: Adicionar arquivo __main__.py e módulo url_fixer.py
    Adiciona o arquivo __main__.py com a função main() que imprime uma mensagem de saudação. Também adiciona o módulo url_fixer.py com funções para corrigir e verificar URLs.

---
+++ theses_scraper/__main__.py
@@ -0,0 +1,6 @@
+def main():
+    print(f"Hello from {__package__}!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ theses_scraper/url_fixer.py
@@ -0,0 +1,148 @@
+"""Módulo com funções para corrigir e verificar URLs"""
+
+from functools import reduce
+
+_DENY_LIST = {
+    "",
+    "localhost:8080",
+    "localhost:443",
+    "152.92.4.120",
+    "link.springer.com",
+    "dx.doi.org.br",
+    "sucupira.capes.gov.br",
+    "linktr.ee",
+    "orcid.org",
+    "lattes.cnpg.br",
+    "orcid. org",
+    "www.youtube.com",
+    "dx.doi.org.",
+    "dx.doi.org",
+    "lattes.cnpq.br",
+    "orcid",
+    "youtu.be",
+    "drive.google.com",
+    "dx,doi.org",
+    "lattes,cnpq.br",
+    "dx:doi.org",
+    "dx.org",
+    "dx;xoi.org",
+    "onlinelibrary.wiley.com",
+    "buscatextual.cnpq.br",
+    "bdtccs.furg.br:8080",
+    "wwws.cnpq.br",
+    "Iattes.cníci.br",
+    "lattes.cnp q.br",
+    "www.abpnrevista.org.br",
+    "www.sciencedirect.com",
+    "sistemas.usp.br",
+    "www.movisaberesefazeresmigrantes.com",
+    "sitios.anhembi.br",
+    "www.bd.bibl.ita.br",
+    "www.bdita.bibl.ita.br",
+    "tede.inatel.br:8080",
+    "riu.ufam.edu.br",
+    "www.teses.ufc.br/",
+    "comum.rcaap.pt",
+    "rubi.casaruibarbosa.gov.br",
+    "dspace7fgv.neki-it.com.br",
+    "repositorio.ismt.pt",
+    "dspace.ismt.pt",
+    "teses.ufrj.br",
+    "www.uff.br",
+    "app.uff.br",
+    "http://tede.fecap.br/",
+    "www2.unifesp.br",
+    "www.egn.mar.mil.br",
+    "repositorioinstitucional.uea.edu.br",
+    "bibliotecadigital.unec.edu.br",
+    "repositorio.santamariasaude.pt",
+    "www.defesa.gov.br",
+    "teses2.ufrj.br",
+    "ufba.academia.edu",
+    "biotecnologia.ufba.br",
+    "repositorio.faema.edu.br:8000",
+    "ppgss.ufsc.br",
+    "unesp.primo.exlibrisgroup.com",
+    "repositorio.fgv.br",
+    "www.repositorio.unicamp.br",
+    "repositorio.uenp.edu.br",
+}
+
+_REPLACEMENTS = {
+    "repositorio.lnec.pt:8080": "repositorio.lnec.pt",
+    "http://tede2.usc.br:8080": "https://tede2.usc.br:8443",
+    "https://repositorio.ifba.edu.br": "http://repositorio.ifba.edu.br",
+    "https://rigeo.cprm.gov.br": "http://rigeo.cprm.gov.br",
+    "dspace.unipampa.edu.br:8080": "dspace.unipampa.edu.br",
+    "repositorio.unilab.edu.br:8080": "repositorio.unilab.edu.br",
+    "bd.bibl.ita.br": "bdita.bibl.ita.br",
+    "teste.tede.unifacs.br:8080": "tede.unifacs.br",
+    "repositorio.ifap.edu.br:8080": "repositorio.ifap.edu.br",
+    "http://repositorio.ufal.br": "http://www.repositorio.ufal.br",
+    "bibliodigital.unijui.edu.br:8080": "bibliodigital.unijui.edu.br",
+    "bibliodigital.unijui.edu.br/xmlui": "bibliodigital.unijui.edu.br",
+    "vkali40.ucs.br:8080": "repositorio.ucs.br",
+    "10.1.0.96:8080": "repositorio.ifba.edu.br",
+    "prod.repositorio.ufscar.br": "repositorio.ufscar.br",
+    "www7.bahiana.edu.br": "repositorio.bahiana.edu.br",
+    "ri.ucsal.br:8080": "ri.ucsal.br",
+    "ri.ucsal.br/jspui": "ri.ucsal.br",
+    "10.0.217.128:8080": "tede.upf.br",
+    "lrepositorio.ufra.edu.br": "repositorio.ufra.edu.br",
+    "www.repositorio.fjp.mg.gov.br": "repositorio.fjp.mg.gov.br",
+    "rigeoh.cprm.gov.br": "rigeo.cprm.gov.br",
+    "200.129.163.131:8080": "tede.ufam.edu.br",
+    "200.136.52.105": "repositorio.ipen.br",
+    "200.129.209.58:8080": "repositorio.ufgd.edu.br/jspui",
+    "repositorio.ufgd.edu.br/jspui/jspui": "repositorio.ufgd.edu.br/jspui",
+    "200.129.209.58": "repositorio.ufgd.edu.br",
+    "www.repositorio.ufrb.edu.br": "ri.ufrb.edu.br",
+    "repositorio.faema.edu.br:8000": "repositorio.unifaema.edu.br",
+    "www.repositorio.ufba.br": "repositorio.ufba.br",
+    "acervo.ufvjm.edu.br": "repositorio.ufvjm.edu.br",
+    "repositorio.ufvjm.edu.br:8080": "repositorio.ufvjm.edu.br",
+    "repositorio.ufvjm.edu.br/jspui": "repositorio.ufvjm.edu.br",
+    "repositorio.cruzeirodosul.edu.br:8080": "repositorio.cruzeirodosul.edu.br",
+    "repositorio.cruzeirodosul.edu.br": "repositorio.cruzeirodosul.edu.br/jspui",
+    "repositorio.cruzeirodosul.edu.br/jspui/jspui": "repositorio.cruzeirodosul.edu.br/jspui",
+    "tede2.unisagrado.edu.br:8080": "tede2.unisagrado.edu.br:8443",
+    "http://repositorio.ufes.br": "https://repositorio.ufes.br",
+    "tede.utp.br:8080": "tede.utp.br",
+    "tede.unioeste.br:8080/tede": "tede.unioeste.br",
+    "tedebc.ufma.br/jspui/handle/tede/tede": "tedebc.ufma.br/jspui/handle/tede",
+    "www.repositorio.uniceub.br": "repositorio.uniceub.br/jspui",
+    "repositorio.uniceub.br/jspui/jspui": "repositorio.uniceub.br/jspui",
+    "www.repositorio.insper.edu.br": "repositorio.insper.edu.br",
+    "tede2.uefs.br:8080/handle/tede/tede": "tede2.uefs.br:8080/handle/tede",
+    "repositorio.unb.br": "repositorio.unb.br/jspui",
+    "repositorio.unb.br/jspui/jspui": "repositorio.unb.br/jspui",
+    "dspace.idp.edu.br:8080/xmlui": "repositorio.idp.edu.br",
+    "purl.net/esepf/handle": "hdl.handle.net",
+}
+
+
+def update_url(url: str) -> str:
+    """
+    Atualiza uma URL para a versão correta, considerando um conjunto de regras pré-definidas.
+
+    Args:
+        url (str): A URL a ser corrigida.
+
+    Returns:
+        str: A URL corrigida conforme as regras.
+
+    Examples:
+        >>> update_url("http://tede2.usc.br:8080")
+        'https://tede2.usc.br:8443'
+    """
+    return reduce(
+        lambda updated_url, target: updated_url.replace(target, _REPLACEMENTS[target]),
+        _REPLACEMENTS,
+        url,
+    )
+
+
+def is_denied(url: str) -> bool:
+    """Verifica se a url está na lista de urls não permitidas"""
+    base_url = url.split("/")[2]
+    return base_url in _DENY_LIST
